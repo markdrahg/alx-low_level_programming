429ab3f75754457560ad8b50f3aa5ef34e9a9e01
Mark Drah <132355775+markdrahg@users.noreply.github.com>
2023-06-15 14:37:41 +0000

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,5 +17,5 @@ printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(n));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(o));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(p));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(q));
-return (0);
+return 0;
 }

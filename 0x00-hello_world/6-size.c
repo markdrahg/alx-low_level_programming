a2b8b0ca4fa3b54aa7c31dd17fdfe931947ef7cd
Mark Drah <132355775+markdrahg@users.noreply.github.com>
2023-06-15 17:39:49 +0000

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,21 +1,15 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- * main - a program that prints size of data types
- * Return 0 (Success)
- */
-
+* main - A program that prints the size of various Ddata types
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-char m;
-int n;
-long int o;
-long long int p;
-float q;
-
-printf("Size of a char: %d byte(s)\n", (unsigned long)sizeof(m));
-printf("Size of an int: %d byte(s)\n", (unsigned long)sizeof(n));
-printf("Size of a long int: %d byte(s)\n", (unsigned long)sizeof(o));
-printf("Size of a long long int: %d byte(s)\n", (unsigned long)sizeof(p));
-printf("Size of a float: %d byte(s)\n", (unsigned long)sizeof(q));
-return (0);
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	return (0);
 }

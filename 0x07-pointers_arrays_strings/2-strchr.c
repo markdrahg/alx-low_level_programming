879af4a56bfd212a644730c95ad1964fa49e791d
markdrahg <neutral520@gmail.com>
2023-07-03 06:24:34 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strchr - Entry point
+ * @s: input
+ * @c: input
+ * Return: Always 0 (Success)
+ */
+
+char *_strchr(char *s, char c)
+{
+	int m = 0;
+
+	for (; s[m] >= '\0'; m++)
+	{
+		if (s[m] == c)
+			return (&s[m]);
+	}
+	return (0);
+}
+
